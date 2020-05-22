f88873ffa0737ea37d21241d116922059be14b18
Tyler French <tylerfrench89@gmail.com>
2020-05-22 16:05:45 -0700

    first commmit, imports, structure and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "../styles/App.css"
+import { useClothes } from '../hooks'
 
-export default props => {
+
+export default clothingBox => {
+  const { clothes, getClothing } = useClothes()
+  console.log(clothes)
+  useEffect(() => {
+    getClothing()
+  }, [])
   return (
-    <div>
-      <img src="/assets/100_1.jpg" />
-      <h1>Hello World</h1>
+    <div className="clothes-box">
+      <div class="header">
+        <small className="products">
+          <span>16 Product(s) found.</span>
+        </small>
+        <div className="sort">Order by<select>
+          <option value="">Select</option>
+          <option value="lowestprice">Lowest to highest</option>
+          <option value="highestprice">Highest to lowest</option>
+          </select>
+        </div>
+      </div>
+      {clothes.map(item => {
+        return (
+          <div className="item">
+            <img src={item.img.normal} />
+            <h1 className="title">{item.title}</h1>
+            <div>{item.currencyFormat}{item.price}</div>
+            <div>or {item.installments} x${item.price / item.installments}</div>
+            <button className="buy-button">Add to cart</button>
+          </div>
+        )
+      })}
+  
     </div>
   )
 }
---
+++ src/redux/ducks/GetData/index.js
@@ -0,0 +1,47 @@
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import axios from 'axios'
+
+const GRAB_CLOTHES = "example/GRAB_CLOTHES"
+
+// 3. initial state
+const initialState = {
+  clothingData: []
+}
+
+// 4. reducer
+export default (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case GRAB_CLOTHES:
+      return { ...state, clothingData: action.payload }
+    default:
+      return state
+  }
+}
+
+// 5. action creators
+function clothing() {
+  return dispatch => {
+    axios.get('/products').then(resp =>{
+      dispatch({
+        type: GRAB_CLOTHES,
+        payload: resp.data
+      })
+    })
+  }
+}
+
+
+// 6. custom hook
+export function useClothes() {
+  const dispatch = useDispatch()
+  const clothes = useSelector(appState => {
+    return appState.clothesState.clothingData 
+  }) 
+
+  const getClothing = () => dispatch(clothing())
+  
+
+  return { clothes, getClothing }
+}
